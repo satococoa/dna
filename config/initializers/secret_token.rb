4dcf32c07af92d95226cd6a1299777be6410bc75
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-09-19 20:25:31 +0900

    r new dna -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dna::Application.config.secret_token = '929128c2a3c3e6e6ff7ecbf0ba621d9baba1d26da68e4e76925f7282d0c0fb439473735df2c4881e495bc7adf7927fbbd7162772df04c81059fa16b45f177bb9'
